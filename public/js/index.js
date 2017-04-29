3532fe58c37bcb7a518b243b9994eba78acf45d4
Vinto <vintogen@yahoo.co.uk>
2017-04-30 01:19:31 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Vinto',
-    text: 'That works for me'
-  });
 });
 
 socket.on('disconnect', function () {
