e1c5865ffb6fb554126ad7abb57b26232cac35c6
Vinto <vintogen@yahoo.co.uk>
2017-05-01 01:12:41 +0200

    Add css to chat app

--- public/js/index.js
+++ public/js/index.js
@@ -11,7 +11,7 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
   console.log('createMessage', message);
   var li = jQuery('<li></li>');
-  li.text(`${message.from} ${message.text}`);
+  li.text(`${message.from}: ${message.text}`);
 
   jQuery('#messages').append(li);
 });
@@ -30,11 +30,12 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val('');
   });
 });
 
@@ -44,12 +45,16 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
   });
 });
